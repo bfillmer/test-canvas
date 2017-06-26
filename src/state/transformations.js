45b9474895a5deca44e7f7b160f7ed5ecf5a98dd
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-06-26 05:06:39 -0600

    Fully typed transformations.

--- src/state/transformations.js
+++ src/state/transformations.js
@@ -1,11 +1,11 @@
 // @flow
 
 // Update x, y in our matrix array with the delta x/y from moving.
-export const pan = (dx: number, dy: number, matrix: Array<number>) => matrix.map(
+export const pan = (dx: number, dy: number, matrix: number[]): number[] => matrix.map(
   (n: number, i: number) => (i === 4) ? n + dx : (i === 5) ? n + dy : n
 )
 
 // Soom in or out based on the delta of the wheel scroll.
-export const zoom = (scale: number, matrix: Array<number>) => matrix.map(
+export const zoom = (scale: number, matrix: number[]): number[] => matrix.map(
   (n: number, i: number) => n * scale
 )
