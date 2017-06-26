29deda95d81ba1d7fa048ec5eff1a5358736c6fe
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-06-26 07:54:59 -0600

    Comment out types for now.

--- src/state/actions.js
+++ src/state/actions.js
@@ -10,44 +10,39 @@ export const types = {
 }
 
 // FLOW TYPES
-type DragStartAction = {
-  type: string,
-  payload: {
-    x: number,
-    y: number,
-    id: string
-  }
-}
-
-type DragEndAction = {
-  type: string
-}
-
-type DraggedAction = {
-  type: string,
-  payload: {
-    x: number,
-    y: number
-  }
-}
-
-type ZoomedAction = {
-  type: string,
-  payload: {
-    dy: number,
-    id: string
-  }
-}
-
-const startDrag: DragStartAction = createAction(types.DRAG_START, (x: number, y: number, id: string) => ({x, y, id}))
-const endDrag: DragEndAction = createAction(types.DRAG_END)
-const dragging: DraggedAction = createAction(types.DRAGGED, (x: number, y: number) => ({x, y}))
-const zooming: ZoomedAction = createAction(types.ZOOMED, (dy: number, id: string) => ({dy, id}))
+// type DragStartAction = {
+//   type: string,
+//   payload: {
+//     x: number,
+//     y: number,
+//     id: string
+//   }
+// }
+//
+// type DragEndAction = {
+//   type: string
+// }
+//
+// type DraggedAction = {
+//   type: string,
+//   payload: {
+//     x: number,
+//     y: number
+//   }
+// }
+//
+// type ZoomedAction = {
+//   type: string,
+//   payload: {
+//     dy: number,
+//     id: string
+//   }
+// }
 
 // ACTIONS
 export const actions = {
-  startDrag,
-  endDrag,
-  dragging,
-  zooming
+  startDrag: createAction(types.DRAG_START, (x: number, y: number, id: string) => ({x, y, id})),
+  endDrag: createAction(types.DRAG_END),
+  dragging: createAction(types.DRAGGED, (x: number, y: number) => ({x, y})),
+  zooming: createAction(types.ZOOMED, (dy: number, id: string) => ({dy, id}))
 }
