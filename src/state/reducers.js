6f8d6f4563774a814ead6b20ac8d659c08513b49
Bryan Fillmer <bryan.fillmer@gmail.com>
2017-06-27 12:05:44 -0600

    Grid math.

--- src/state/reducers.js
+++ src/state/reducers.js
@@ -7,24 +7,32 @@ import {pan, zoom} from 'state/transformations'
 
 import {DragStartPayload, DraggedPayload, ZoomedPayload, Svg, SvgState} from 'state/types'
 
-export const svgObject = (id: string, matrix: number[] = [1, 0, 0, 1, 0, 0], dragX: number = 0, dragY: number = 0): Svg => ({id, matrix, dragX, dragY})
+export const svgObject = (
+  id: string,
+  type: string,
+  matrix: number[] = [1, 0, 0, 1, 0, 0],
+  dragX: number = 0,
+  dragY: number = 0
+): Svg => ({
+  id,
+  type,
+  matrix,
+  dragX,
+  dragY
+})
+
+const VIEWPORT = 'VIEWPORT'
+const BLOCK = 'BLOCK'
 
 // INITIAL STATE
 const initialState: SvgState = {
   dragging: false,
   scalar: 1,
   svg: [
-    svgObject('viewport'),
-    svgObject('circle-one', [1, 0, 0, 1, -10, -10]),
-    svgObject('circle-two', [1, 0, 0, 1, -10, 40]),
-    svgObject('circle-three', [1, 0, 0, 1, -10, -60]),
-    svgObject('circle-four', [1, 0, 0, 1, 80, -10]),
-    svgObject('circle-five', [1, 0, 0, 1, 80, 40]),
-    svgObject('circle-six', [1, 0, 0, 1, 80, -60]),
-    svgObject('circle-seven', [1, 0, 0, 1, 35, -10]),
-    svgObject('circle-eight', [1, 0, 0, 1, 170, -10]),
-    svgObject('circle-nine', [1, 0, 0, 1, 170, 40]),
-    svgObject('circle-ten', [1, 0, 0, 1, 170, -60])
+    svgObject('viewport', VIEWPORT),
+    svgObject('block-one', BLOCK, [1, 0, 0, 1, -10, -10]),
+    svgObject('block-two', BLOCK, [1, 0, 0, 1, -10, 40]),
+    svgObject('block-three', BLOCK, [1, 0, 0, 1, -10, -60])
   ]
 }
 
--- src/state/store.js
+++ src/state/store.js
@@ -2,6 +2,9 @@
 import {createStore, compose} from 'redux'
 
 import {reducer} from 'state/reducers'
+import {grid} from 'state/grid'
+
+console.log(grid)
 
 // Use Redux DevTools Extension if available and not in production.
 const composeEnhancers = ((process.env.NODE_ENV !== 'production') && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
